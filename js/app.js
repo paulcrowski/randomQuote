18f44c3b2e24b7744d8f8fb0b51f92dc83fd7fea
Pawel <p.p.wronski@gmail.com>
2016-06-22 14:29:31 +0200

    random quote AJAX, random quote button

--- js/app.js
+++ js/app.js
@@ -4,16 +4,50 @@
 console.log('test');
 
 $(document).ready(function() {
-    geo();
+    getQuote();
+    buttonRandom();
 });
 
 
-function weather (lat,lon) {
-
-
-    $.getJSON(link, showWeather);
+function getQuote () {
+//  var link = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback";
+    var link = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
+    console.log(link);
+    $.getJSON(link, showQuote);
 }
 
-function showWeather(weatherReport) {
+function showQuote(quoteData) {
+    console.log("---------------- data")
+    var title = quoteData[0].title;
+    var content = quoteData[0].content;
+    var quote = $('#quote');
+    var name = $('#name');
+    // add content and title to web page
+    quote.html(content);
+    name.html(title);
+    // show box with content with animation
+    colorBackground();
+    $('.box').fadeIn('slow');
+    console.log(content);
+    console.log(title);
+}
 
+function colorBackground () {
+    var colors = ["#9B59B6","#77B1A9","#73A857", "#2C3E50", "#FB6964"];
+    var randomColor = randomIntFromInterval(0,colors.length-1);
+    var body =$('body');
+    console.log(colors[randomColor]);
+    body.css("background-color",colors[randomColor]).fadeIn("slow");
+}
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function buttonRandom () {
+    $('.buttonRandom').click(function(){
+        console.log('click');
+        $('body').css("display","none");
+        getQuote();
+    })
 }
